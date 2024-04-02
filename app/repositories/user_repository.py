95bc5da21adec14363d1e3f47b81ce90fe72614f
Rafael Acílio de Figueiredo <rafaelacilio@hotmail.com>
2024-04-02 08:53:34 -0300

    02/04 - ajuste login

--- app/repositories/user_repository.py
+++ app/repositories/user_repository.py
@@ -1,4 +1,6 @@
-from app.entities.user import UserDb
+from typing import Optional
+
+from app.entities.user import UserDb, UserLogin
 from sqlalchemy.orm import Session
 
 
@@ -7,3 +9,16 @@ def create_user_db(new_user: UserDb, db: Session):
     db.commit()
     db.refresh(new_user)
     return new_user
+
+
+def get_user_by_name(username: str, db: Session) -> Optional[UserLogin]:
+    user = (
+        db.query(UserDb)
+        .filter(UserDb.username == username)
+        .first()
+    )
+
+    if user is None:
+        return None
+
+    return UserLogin(id=user.id, username=user.username, email=user.email, rule=user.rule.value, password=user.password)
--- app/routes/user_router.py
+++ app/routes/user_router.py
@@ -1,9 +1,16 @@
+from typing import Annotated
+
+import bcrypt
+from fastapi.security import OAuth2PasswordRequestForm
+
+from app.services.auth_service import create_jwt_token
+
 from app.database import get_db
-from app.repositories.user_repository import create_user_db
+from app.repositories.user_repository import create_user_db, get_user_by_name
 from app.entities.user import UserDb
 from app.services.user_service import create_password_hash
 from sqlalchemy.orm import Session
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 
 router = APIRouter(
     prefix='/users',
@@ -18,3 +25,23 @@ def create_user(username: str, email: str, password: str,
     new_user = UserDb(username=username, email=email, password=password)
     created_user = create_user_db(new_user, db)
     return created_user
+
+
+@router.post("/login")
+def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: Session = Depends(get_db)):
+    user = get_user_by_name(form_data.username, db)
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    if bcrypt.checkpw(form_data.password.encode('utf-8'), user.password.encode('utf-8')):
+        token_data = {
+            "id": user.id,
+            "username": user.username,
+            "email": user.email,
+            "rule": user.rule
+        }
+
+        access_token = create_jwt_token(data=token_data)
+        return access_token
+    else:
+        raise HTTPException(status_code=401, detail="Invalid credentials")
